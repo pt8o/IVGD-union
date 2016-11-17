2fac18a771ec37435133b77762542b57385a5e42
Ana TM <ana.taveramendoza@gmail.com>
2016-11-16 22:25:02 -0500

    released a live html example of the dynamic lighting, for documentation purposes
    https://alfalfasalads.itch.io/lighting-test?secret=hdg8VavtN6klCKBXQuv6bdv6k

---
+++ Assets/Scripts/mouseFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//quick little script for the lighting example.
+public class mouseFollow : MonoBehaviour {
+
+   
+
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Translate(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0);
+
+
+    }
+}
