6e80f23a5bf039c42e7bba5f78cd7d58e6dab8dd
superko <lucas@superko.org>
2016-11-22 21:25:12 -0500

    Proof-of-concept of level rotation, active platforms
    - A/D or left/right to move player
    - mouse left/right rotates the currently active platform
    - currently active platform determined by the platform last triggered by
    player

---
+++ Assets/Testing-Lucas/Scripts/GodControl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GodControl : MonoBehaviour {
+
+	public float horizontalSpeed = 5.0F;
+	public GameObject platform;
+
+	void Update() {
+		float h = horizontalSpeed * Input.GetAxis("Mouse X");
+		platform.transform.Rotate(0, 0, -h);
+	}
+}
---
+++ Assets/Testing-Lucas/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+
+		rb.AddForce (movement * speed);
+	}
+}
